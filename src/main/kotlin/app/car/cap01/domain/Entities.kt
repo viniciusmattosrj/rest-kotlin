6e2c6f81d8d201ac82528e47ddff82bc0962f891
Vinicius Mattos <vinicius.mattos@zoop.com.br>
2021-09-04 01:28:37 -0300

    04092021 - #01 o que é rest

---
+++ src/main/kotlin/app/car/cap01/domain/Entities.kt
@@ -0,0 +1,14 @@
+package app.car.cap01.domain
+
+import java.time.LocalDate
+import javax.lang.model.element.Name
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
+data class Driver(
+    @Id
+    var id: Long? = null,
+    val name: String,
+    val birthDate: LocalDate
+)
